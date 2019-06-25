29bdcc61ac01795931c26ed7ffc8d12ce8164191
Maksym Dakal <dakalm@gmail.com>
2019-06-25 14:09:22 +0300

    Created and first run AppTwo

--- ProTwo/AppTwo/urls.py
+++ ProTwo/AppTwo/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-
+from .views import home
 
 urlpatterns = [
-    path('', include('AppTwo.urls')),
-    path('admin/', admin.site.urls),
+    path('home/', home, name='home')
 ]
--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'AppTwo/index.html', context={'name': 'Django!'})
