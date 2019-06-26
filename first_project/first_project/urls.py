56c7aa58e79168d9762c2828b61326918e996ac9
Maksym Dakal <dakalm@gmail.com>
2019-06-26 10:42:51 +0300

    update first project

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -1,7 +1,7 @@
 """first_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -20,5 +20,5 @@ from first_app import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('first_app/', include('first_app.urls')),
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
 ]
