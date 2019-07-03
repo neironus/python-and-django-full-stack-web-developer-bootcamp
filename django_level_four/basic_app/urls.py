ac699fd3cf8e14375cbc77cdfe66b742eb2bcaca
maksym_dakal <dakalm@gmail.com>
2019-07-03 23:17:37 +0300

    url template tagging

---
+++ django_level_four/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('index', views.index, name='index'),
+    path('other', views.other, name='other'),
+    path('relative', views.relative, name='relative')
+]
