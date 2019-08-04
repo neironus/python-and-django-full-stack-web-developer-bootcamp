5b2743c82abdddc66aff950ef1fb4de605c023e3
maksym_dakal <dakalm@gmail.com>
2019-08-05 00:26:54 +0300

    *

--- django_level_five/learning_users/basic_app/views.py
+++ django_level_five/learning_users/basic_app/views.py
@@ -3,6 +3,9 @@ from .forms import UserForm, UserProfileForm
 
 # Create your views here.
 def home(request):
+    print('*' * 5)
+    print(request.META.get('HTTP_X_REAL_IP') or request.META.get('REMOTE_ADDR'))
+    print('*' * 5)
     return render(request, 'basic_app/index.html',
             {
                 'user_form': UserForm,
