3e230604a0de55beffccf0a3bb492094e3fdf4f4
maksym_dakal <dakalm@gmail.com>
2019-08-10 01:43:05 +0300

    addbasic html template and login page with controller

--- django_level_five/learning_users/basic_app/urls.py
+++ django_level_five/learning_users/basic_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, login_user, sign_up
 
-
+app_name = 'basic_app'
 urlpatterns = [
     path('', home, name='home'),
+    path('login', login_user, name='login-user'),
+    path('sign-up', sign_up, name='sign-up')
 ]
--- django_level_five/learning_users/basic_app/views.py
+++ django_level_five/learning_users/basic_app/views.py
@@ -1,12 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .forms import UserForm, UserProfileForm
 
 # Create your views here.
 def home(request):
-    print('*' * 5)
-    print(request.META.get('HTTP_X_REAL_IP') or request.META.get('REMOTE_ADDR'))
-    print('*' * 5)
-    return render(request, 'basic_app/index.html',
+    if request.user.is_authenticated:
+        print(request.user)
+    else:
+        print(None)
+    # print('*' * 5)
+    # print(request.META.get('HTTP_X_REAL_IP') or request.META.get('REMOTE_ADDR'))
+    # print('*' * 5)
+    return render(request, 'basic_app/index.html')
+
+def login_user(request):
+    # if request.user.is_authenticated:
+    #     print(request.user)
+    #     print(request)
+    # else:
+    #     print(None)
+    if request.method == 'POST':
+        username = request.POST.get('login')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            print(f"Hello {login}")
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse("basic_app:home"))
+    return render(request, 'basic_app/login.html')
+    
+def sign_up(request):
+    return render(request, 'basic_app/signup.html',
             {
                 'user_form': UserForm,
                 'user_profile': UserProfileForm
