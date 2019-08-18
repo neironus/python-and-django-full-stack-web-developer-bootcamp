cc33fce8b9f606bc494541f7a0eb35ddaaa20c30
maksym_dakal <dakalm@gmail.com>
2019-08-19 00:05:55 +0300

    usersprofile

--- django_level_five/learning_users/basic_app/urls.py
+++ django_level_five/learning_users/basic_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 # from django.contrib.auth import 
-from .views import home, login_user, sign_up, logout_user
+from .views import home, login_user, sign_up, logout_user, profile_user
 
 app_name = 'basic_app'
 urlpatterns = [
     path('', home, name='home'),
     path('login', login_user, name='login-user'),
     path('sign-up', sign_up, name='sign-up'),
-    path('logout', logout_user, name='logout-user')
+    path('logout', logout_user, name='logout-user'),
+    path('profile/<str:user_name>', profile_user, name='profile-user')
 ]
--- django_level_five/learning_users/basic_app/views.py
+++ django_level_five/learning_users/basic_app/views.py
@@ -1,27 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .forms import UserForm, UserProfileForm
 
 # Create your views here.
 def home(request):
-    # if request.user.is_authenticated:
-    #     print(request.user)
-    # else:
-    #     print(None)
-    # print(dir(request.user))
-    # print('*' * 5)
-    # print(request.META.get('HTTP_X_REAL_IP') or request.META.get('REMOTE_ADDR'))
-    # print('*' * 5)
+    # profile_user(request)
     context = {'user': request.user}
     return render(request, 'basic_app/index.html', context)
 
 def login_user(request):
+    print(request.GET.get('key'))
     if request.method == 'POST':
         username = request.POST.get('login')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
+        # print(request.POST)
         if user is not None:
             if user.is_active:
                 login(request, user)
@@ -37,7 +33,6 @@ def logout_user(request):
     
 def sign_up(request):
     """
-    * Создание пользователя +
     * Переадресация пользователя на страничу входа
     """
     if request.method == 'POST':
@@ -48,46 +43,29 @@ def sign_up(request):
         user_form = UserForm(data=request.POST)
         user_profile = UserProfileForm(data=request.POST)
         if user_form.is_valid() and user_profile.is_valid():
-            print('*****************')
-            print(user_form.cleaned_data['username'])
+            # print('*****************')
+            # print(user_form.cleaned_data['username'])
             user = user_form.save()
             user.set_password(user.password)
             user.save()
             profile = user_profile.save(commit=False)
             profile.user = user
             profile.save()
-            print('*****************')
+            # print('*****************')
             return HttpResponseRedirect(reverse("basic_app:login-user"))
-        
-        '''
-        if request.method == 'POST':
-            # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'name.html', {'form': form})
-        '''
-        # print(user)
-        # UserForm = UserForm()
-        # return render(request, 'basic_app/signup.html',
-        #         {
-        #             'user_form': UserForm,
-        #             'user_profile': UserProfileForm
-        #         }
-        #     )
-    # UserForm = UserForm()
     return render(request, 'basic_app/signup.html',
                 {
                     'user_form': UserForm,
                     'user_profile': UserProfileForm
                 }
             )
+
+def profile_user(request, user_name):
+    # user_name=request.user
+    print('*****************')
+    print(user_name)
+    user = get_object_or_404(User, username=user_name)
+    print('*****************')
+
+    # user = User.objects.get(username)
+    return render(request, 'basic_app/user-profile.html', {'user':user})
